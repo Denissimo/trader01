b926f565e6c0f68d822a2e5ccf46fc2e3df2f2c1
unknown <d.drozdeckiy@voltron.local>
2021-09-08 15:13:14 +0300

    start page

---
+++ src/Controller/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class TestController extends AbstractController
+{
+    public function buildContent(Request $request)
+    {
+        return $this->render('lk.html.twig', [
+            'id' => 155,
+            'use_arcgis' => false
+        ]);
+    }
+
+}
